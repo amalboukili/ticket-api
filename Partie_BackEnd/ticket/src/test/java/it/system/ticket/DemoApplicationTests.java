474a2f9d79bdbc5e02101efd3f379d736140c2a8
amalboukili <boukili.amal@gmail.com>
2019-12-08 14:25:47 +0000

    first commit_BackEnd

---
+++ Partie_BackEnd/ticket/src/test/java/it/system/ticket/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.system.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
